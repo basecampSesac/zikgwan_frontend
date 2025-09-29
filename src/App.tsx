3e63293560876cce06aea9ed52583d8ccb5505c1
doyeonkim1997 <0104801@naver.com>
2025-09-29 23:58:04 +0900

    feat: 신규 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import TicketList from "./page/TicketList";
 import GroupList from "./page/GroupList";
 import LoginPage from "./page/Login";
 import SignupPage from "./page/SignUp";
+import SchedulePage from "./page/Schedule";
+import PolicyPage from "./page/Policy";
+import TicketGuidePage from "./page/TicketGuide";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,9 @@ const router = createBrowserRouter([
       { path: "/groups", Component: GroupList },
       { path: "/login", Component: LoginPage },
       { path: "/signup", Component: SignupPage },
+      { path: "/schedule", Component: SchedulePage },
+      { path: "/policy", Component: PolicyPage },
+      { path: "/ticket-guide", Component: TicketGuidePage },
     ],
   },
 ]);
