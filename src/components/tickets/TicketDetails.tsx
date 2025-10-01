eec848cd303af0a2f63a9858da7e1acd91a0aee0
doyeonkim1997 <0104801@naver.com>
2025-10-01 15:52:47 +0900

    refactor: GroupCard, TicketCard, TicketDetails, Modal 컴포넌트 폴더 구조 정리

---
+++ src/components/tickets/TicketDetails.tsx
@@ -0,0 +1,15 @@
+export default function TicketDetails() {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 id="modal-title" className="text-xl font-semibold">
+        LG 트윈스 vs. 두산 베어스
+      </h2>
+      <p id="modal-description" className="mt-2 text-neutral-600">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel libero
+        aliquam eaque quis ab voluptatem veniam, obcaecati officiis
+        reprehenderit distinctio alias nostrum, quod et assumenda voluptatum
+        sint laborum optio sequi.
+      </p>
+    </div>
+  );
+}
