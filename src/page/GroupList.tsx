46ca53ceb880db723dffe52b2d46670be6276b24
doyeonkim1997 <0104801@naver.com>
2025-10-04 03:30:15 +0900

    feat: 모임 등록 모달 기능 추가

--- src/page/GroupList.tsx
+++ src/page/GroupList.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import SearchPanel from "../components/SearchPanel";
 import GroupCard from "../components/groups/GroupCard";
 import ListHeader from "../components/ListHeader";
-import GroupDetails from "../components/groups/GroupDetails";
+import GroupForm from "../components/groups/GroupForm";
+import Modal from "../components/Modal";
 import { groupsMock } from "../data/mock";
 import type { GroupUI } from "../types/group";
 
 export default function GroupList() {
   const groups: GroupUI[] = groupsMock;
+  const [isCreateOpen, setIsCreateOpen] = useState(false);
+
   return (
     <div className="bg-white min-h-screen">
       <div className="max-w-6xl mx-auto px-6 py-8">
@@ -22,7 +26,7 @@ export default function GroupList() {
             count={groups.length}
             sortOptions={["최신순", "인원 많은순", "인원 적은순"]}
             buttonText="+ 모임 등록"
-            modalChildren={<GroupDetails />}
+            onButtonClick={() => setIsCreateOpen(true)}
           />
         </div>
 
@@ -33,6 +37,11 @@ export default function GroupList() {
           ))}
         </div>
       </div>
+
+      {/* 등록 모달 */}
+      <Modal isOpen={isCreateOpen} onClose={() => setIsCreateOpen(false)}>
+        <GroupForm mode="create" onClose={() => setIsCreateOpen(false)} />
+      </Modal>
     </div>
   );
 }
