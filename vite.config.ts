bdb14dde90f9bd9be85d85833f5896f698f5ec8d
doyeonkim1997 <0104801@naver.com>
2025-10-08 00:35:53 +0900

    fix: SockJS 실행 시 global is not defined 오류 해결 (Vite define 설정 추가)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    global: "window", // global을 브라우저용 window로 매핑
+  },
 });
